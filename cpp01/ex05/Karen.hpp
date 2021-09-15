bf229fd023b86dc028ec4b6c9af16de370549151
Liza Bikineyeva <Lisssaney@BikineevMacBookPro.local>
2021-09-15 16:20:47 +0300

    add MAkefile

--- cpp01/ex05/Karen.hpp
+++ cpp01/ex05/Karen.hpp
@@ -15,6 +15,7 @@ class Karen
 		//char str[4];
 		//str[0] = debug(void);
 		void complain( std::string level );
+		void	(Karen::*choose_lev[4])(void);
 		//void go_comp(int i);
 		//fun _func[4];
 		Karen(void);
---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,14 @@
+# include "Karen.hpp"
+
+int main()
+{
+	Karen karen;
+
+	karen.complain("DEBUG");
+	std::cout << std::endl;
+	karen.complain("INFO");
+	std::cout << std::endl;
+	karen.complain("WARNING");
+	std::cout << std::endl;
+	karen.complain("ERROR");
+}
