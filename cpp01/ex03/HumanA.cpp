b4a3a9b9e65c11b16fb12490f5ae25d82cb34280
Gormon Bump <gbump@mi-u5.msk.21-school.ru>
2021-09-17 05:28:47 +0300

    k

--- cpp01/ex03/HumanA.cpp
+++ cpp01/ex03/HumanA.cpp
@@ -5,4 +5,4 @@ void HumanA::attack(void)
 	std::cout << this->name << " attacks with his " << this->weaponA.getType() << std::endl;
 }
 
-HumanA::HumanA(std::string const &name1, Weapon const &weapon1) : name(name1), weaponA(weapon1){}
+HumanA::HumanA(std::string const &name1, Weapon const &weapon1) : name(name1), weaponA(weapon1){} // оператор присваивания
